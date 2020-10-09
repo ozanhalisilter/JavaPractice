1282a223d2d772204c053449ea6b23565920aa48
Ozan Halis Ilter <ozanhalisilter@gmail.com>
2020-10-09 12:38:30 +0300

    String Literals and Mathematical Operations

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,8 +1,15 @@
 public class HelloWorld {
 
-    public static void main(String[] args) {
-        System.out.println("Hello world");
+    //String Literal = "Text" Cannot be changed unlike variables
 
+
+
+
+
+    public static void main(String[] args) {
+        System.out.println("Hello, Ozan"); //String Literal
+        int myFirstNumber = (5 +5 ) * 4; // Variable assignment
+        System.out.println(myFirstNumber); //Printing variable
     }
 
 }
